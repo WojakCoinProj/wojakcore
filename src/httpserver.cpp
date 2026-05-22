02f2982a2e2d65af6fa29b3bc215e12a8eea0ebf
root <root@vscode.betoglou.net>
2026-05-22 18:26:49 +0000

    fix: add missing includes for GCC 14 / Debian Trixie compatibility
    - httpserver.cpp: add #include <deque> (implicit include removed in GCC 14)
    - bench/bench.h: add #include <limits> for std::numeric_limits

--- src/httpserver.cpp
+++ src/httpserver.cpp
@@ -36,6 +36,7 @@
 
 #include <boost/algorithm/string/case_conv.hpp> // for to_lower()
 #include <boost/foreach.hpp>
+#include <deque>
 #include <boost/scoped_ptr.hpp>
 
 /** Maximum size of http request (request line + headers) */
